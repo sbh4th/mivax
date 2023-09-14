53e588b628d2f4b2e6a9dffe2c36024498549a94
Sam Harper <sbh4th@gmail.com>
2023-09-14 17:40:06 -0400

    Create initial code for Lorenz curve

--- code/prelim-analysis.R
+++ code/prelim-analysis.R
@@ -1,13 +1,26 @@
+#  program:  prelim-analysis.R
+#  task:     preliminary analysis tables for vaccination
+#  input:    svi-sdi.dta, coverage.xlsx, week-15-administered.xlsx 
+#  output:   none
+#  project:  mivax
+#  author:   sam harper \ 2023-09-14
+
+##### 0 #####
+##### load libraries
 library(here)
 library(tidyverse)
 library(readxl)
 library(lubridate)
 library(tsibble)
 library(haven)
+library(kableExtra)
+library(gglorenz)
 
-# read in data
-# get county disadvantage index
-dd <- read_dta("data/svi-sdi.dta")
+##### 1 #####
+##### generate county disadvantage index
+
+# read in county data
+dd <- read_dta(here("data", "svi-sdi.dta"))
 
 # create quintiles of SVI and SDI scores
 dq <- dd %>% rename_all(tolower) %>%
@@ -28,8 +41,11 @@ dq %>% group_by(sviq) %>%
   kable_styling()
 
 
+##### 2 #####
+##### generate vaccine rates
+
 # vaccine numerators
-vn <- read_excel("data/week-15-administered.xlsx", 
+vn <- read_excel(here("data", "week-15-administered.xlsx"), 
   col_names = c("county", "lhd", "raceeth", "agegp",
     "sex", "facility", "doses", "date"), skip = 1)
 
@@ -55,7 +71,7 @@ vns <- vn %>% select(county, raceeth, agegp, doses) %>%
 
 # vaccine denominators
 
-vd <- read_excel("data/coverage.xlsx",
+vd <- read_excel(here("data", "coverage.xlsx"),
   col_names = c("season", "county", "lhd", "raceeth",
     "agegp", "sex", "month", "flu_week", "pop",
     "coverage", "date"), skip = 1)
@@ -87,6 +103,9 @@ vds <- vd %>%
   summarise(pop = sum(mp))
   
 
+##### 3 #####
+##### merge vaccine and county data
+
 # combine vaccine doses and population by county and race
 vax <- vns %>% 
   left_join(vds, by = join_by(county, race)) %>%
@@ -101,20 +120,12 @@ vax <- vns %>%
 # now collapse to calculate vaccine rates
 # by race-ethnicity and deprivation
 
-t <- vax %>% group_by(sviq, racef) %>%
-  summarise(doses = sum(doses), 
-            pop = sum(pop)) %>%
-  mutate(rate = doses / pop * 100)
 
-vax %>% group_by(sdiq, race) %>%
-  summarise(doses = sum(doses), 
-            pop = sum(pop)) %>%
-  mutate(rate = doses / pop * 100) %>%
-  filter(race!=5) %>%
-  ggplot(aes(x = sdiq, y = rate, group=race, color=race)) + geom_line() 
+##### 4 #####
+##### generate initial tables of vaccine rates
+##### by race-ethnicity and disadvantage quintiles
 
-library(kableExtra)
-
+# vaccine rates by SVI quintile
 t1 <- vax %>% filter(race!=5) %>%
   group_by(racef,sviq) %>%
   summarise(doses = sum(doses), 
@@ -131,6 +142,9 @@ t1 %>%
   add_header_above(c(" " = 1, "SVI Quintile (1=lowest)" = 5)) %>%
   kable_classic(html_font = "Helvetica", full_width = F) %>%
   footnote(general = "SVI quintiles based on unweighted distribution across counties.")
+
+
+# vaccine rates by SDI quintile
 
 t2 <- vax %>% filter(race!=5) %>%
   group_by(racef,sdiq) %>%
@@ -151,8 +165,47 @@ t2 %>%
 
 
 
+f1 <- vn %>% select(county, doses) %>%
+  group_by(county) %>%
+  summarise(tdoses = sum(doses)) %>%
+  right_join(dq, by = join_by(county)) %>%
+  mutate(svi_rate = tdoses / svi_totpop * 100,
+         sdi_rate = tdoses / sdi_county_population * 100)
 
-%>%
-  mutate(rate = doses )
+
+svi_label <- "Least vaccinated 50%\nof counties are 42% of\noverall vaccination rate"
+
+f1 %>%
+  arrange(svi_rate) %>%
+  mutate(cpc = rank(svi_rate)/length(svi_rate)*100,
+         cpv = cumsum(svi_rate)/sum(svi_rate)*100) %>%
+  ggplot(aes(x = cpc, y = cpv, colour="coverage")) + geom_line() +
+  geom_segment(aes(x = 0, y = 0, xend = 100, yend = 100,
+    colour = "equality"), linetype = 'dashed') +
+  coord_cartesian(expand=FALSE) +
+  scale_x_continuous(limits = c(0,100)) +
+  scale_y_continuous(limits = c(0,100)) +
+  scale_color_manual(values = c("#377eb8", "black")) +
+  theme_bw() +
+  geom_curve(aes(x = 53, y = 32, xend = 50, yend = 40),
+    curvature = -0.3, arrow = arrow(length = unit(0.02, "npc"))) +
+  geom_text_npc(aes(npcx = 0.9, npcy = 0.2, label = svi_label),
+                color = "#377eb8") +
+  labs(x = "Cumulative Percentage of Counties, Ranked by Vaccine Coverage",
+    y = "Cumulative Percentage of Vaccine Coverage",
+    title = "Lorenz Curve for Relative Inequality by SVI")
+
+
+   ggplot(aes(svi_rate)) +
+    stat_lorenz(desc = TRUE) +
+    coord_fixed() +
+    geom_abline(linetype = "dashed") +
+    theme_minimal() +
+    # scale_x_percent() +
+    # scale_y_percent() +
+    # theme_ipsum_rc() +
+    labs(x = "Cumulative Percentage of Counties, Ranked by Vaccine Coverage",
+         y = "Cumulative Percentage of Vaccine Coverage",
+         title = "Inequality Among Counties")
 
 
